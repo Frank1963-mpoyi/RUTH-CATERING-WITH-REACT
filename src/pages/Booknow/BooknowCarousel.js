51faa8bd1bf64dbf5e7826be7ca07114c44efd95
Mpoyi-Tshibuyi <mpoyitshibuyi63@gmail.com>
2021-04-16 15:04:18 +0200

    our services

--- src/pages/Booknow/BooknowCarousel.js
+++ src/pages/Booknow/BooknowCarousel.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import "./Booknow.css"
-import mylogo from "./images/ruh.jpg";
+
 
 
 export default function Booknow () {
     return (
-        <>
-        <img src={mylogo} style={{width: "100%", height: "100vh"}} alt="" />
-            <h1 className='contact'>Book Now is Under Construction</h1>
-        </>
+                
+        <section id="contact-section"  >
+        <div className="container-fluid">
+            <h2>Book Our Services</h2>
+            <p>Email us and keep up to date with our latest news </p>
+            <div className="contact-form">
+
+                 {/* first grid section  */}
+                <div>
+                    <i className="fa fa-map-marker"></i><span className="form-info"> Brackenfell Cape Town South Africa </span><br/>
+                    <i className="fa fa-phone"></i><span className="form-info">Phone: + (27) 736485511 </span><br/>
+                    <i className="fa fa-envelope"></i><span className="form-info">ruthmpoyi63@gmail.com</span><br/>
+                </div>
+
+                 {/* Second grid   */}
+                <div>
+                    <form action="">
+                        <input type="text" placeholder="First Name" required/>
+                        <input type="text" placeholder="Last Name" required/>
+                        <input type="Email" placeholder="Email" required/>
+                        <input type="text" placeholder="Subject of this message" required/>
+                        <textarea nameName="message" id="" placeholder="Message" rows="5" required></textarea>
+                        <button type="submit" className="submit">Send Message</button>
+                    </form>
+                </div>
+            </div>
+        
+        </div>
+    </section>
     );
 }
